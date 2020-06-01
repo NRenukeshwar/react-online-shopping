36997b5ec348095c9be002baa88855c34999e19f
NRenukeshwar <noolurenu.97@gmail.com>
2020-06-01 22:27:50 +0530

    2 files modified

--- AboutUs.js
+++ AboutUs.js
@@ -3,8 +3,14 @@ import React from 'react'
 function AboutUs()
 {
   return(
-    <div>
-    About
+    <div className="container text-center">
+      <p className="text-warning font-weight-bold mt-5 mb-0" style={{fontSize:"80px"}}>NN 
+      <span className="text-success font-weight-light mb-0" style={{fontSize:"40px"}}> Shopping</span></p>
+      <p className="text-primary font-weight-bold">Since 1997</p>
+      <hr style={{borderTop:"1px solid skyblue"}}/>
+      <div className="m-5">
+      <p>NN Shopping is an Indian e-commerce company based in Bengaluru, Karnataka, India. It was founded by NN. The company product categories such as consumer electronics, fashion, home essentials & groceries, and lifestyle products. Mobiles in Electronics stands first in sales in our e-commerce. Our Success, because of our thousands of employees. At NN India hundreds of disabled associates find equal opportunity and a community they relate with. This year 2020, We introduced 24x7 Support.</p>
+      </div>
     </div>
   )
 }
--- ContactUs.js
+++ ContactUs.js
@@ -3,8 +3,25 @@ import React from 'react'
 function ContactUs()
 {
   return(
-    <div>
-    Contact
+    <div className="container text-center">
+      <p className="text-warning font-weight-bold mt-5" style={{fontSize:"80px",marginBottom:"0px"}}>NN 
+      <span className="text-success font-weight-light" style={{fontSize:"40px"}}> Shopping</span></p>
+      <hr className="m-3" style={{borderTop:"1px solid skyblue"}}/>
+      <br/>
+      <p className="text-info">You can contact us through...</p>
+      
+      <p className="m-2"> write to <span><i className="fas fa-envelope m-2" style={{fontSize:"20px",color: "#139DF9"}}></i></span> NNShopping@gmail.com</p>
+      <br/>
+      <div className="row ml-5 mr-5">
+        <hr style={{width:"45%",borderTop:"1px solid gold"}}/>
+        <label className="font-weight-bold">OR</label>
+        <hr style={{width:"45%",borderTop:"1px solid gold"}}/>
+      </div>
+      <div>
+      <p className="mt-3" style={{color:"#4138BE"}} title="Facebook"><span><i className="fab fa-facebook" style={{fontSize:"40px"}}></i></span> Facebook</p>
+      <p style={{color: "#FF0000"}} title="Google"><span><i className="fab fa-google-plus-square" style={{fontSize:"40px"}}></i></span> Google</p>
+      <p style={{color: "#139DF9"}} title="Twitter"><span><i className="fab fa-twitter-square" style={{fontSize:"40px"}}></i></span> Twitter</p>
+      </div>
     </div>
   )
 }
